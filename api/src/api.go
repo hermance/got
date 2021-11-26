f0fffd0f5e774651180275e1613f42d638b3c4af
Nils Napoli <nils.napoli.ext@squirel.fr>
2021-11-26 14:06:45 +0100

    Added basic api model

---
+++ api/src/api.go
@@ -0,0 +1,57 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"os"
+
+	"github.com/gin-gonic/gin"
+	"github.com/joho/godotenv"
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
+)
+
+func main() {
+	load_env()
+	db := connect_to_db()
+	migrate(db)
+	listen(db)
+}
+
+func listen(db *gorm.DB) {
+	r := gin.Default()
+
+	r.GET("/ping", func(c *gin.Context) {
+		c.JSON(200, gin.H{
+			"message": "pong",
+		})
+	})
+
+	r.Run()
+}
+
+func connect_to_db() *gorm.DB {
+	database := os.Getenv("MYSQL_DATABASE")
+	host := os.Getenv("MYSQL_HOST")
+	password := os.Getenv("MYSQL_PASSWORD")
+	port := os.Getenv("MYSQL_PORT")
+	user := os.Getenv("MYSQL_USER")
+	dsn := fmt.Sprintf("%s:%s@tcp(%s:%s)/%s?charset=utf8mb4&parseTime=True&loc=Local", user, password, host, port, database)
+	log.Println(dsn)
+	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
+	if err != nil {
+		panic("failed to connect database")
+	}
+	return db
+}
+
+func load_env() {
+	err := godotenv.Load()
+	if err != nil {
+		panic("Error loading .env file")
+	}
+}
+
+func migrate(db *gorm.DB) {
+	db.AutoMigrate(&Organisation{}, &User{}, &UserConfig{}, &WorkDay{})
+}
---
+++ api/src/models.go
@@ -0,0 +1,25 @@
+package main
+
+import "gorm.io/gorm"
+
+type Organisation struct {
+	gorm.Model
+	name string
+}
+
+type User struct {
+	gorm.Model
+	email    string
+	name     string
+	password string
+}
+
+type UserConfig struct {
+	gorm.Model
+}
+
+type WorkDay struct {
+	gorm.Model
+	date     string
+	day_type string // on_site / remote / off
+}
